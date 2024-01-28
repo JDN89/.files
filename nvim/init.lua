565b2a0e065a5e08779380180b087d7518ee36f3
JDN89 <jan.de.niels@gmail.com>
2024-01-28 12:13:59 +0100

    update nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -302,6 +302,13 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnos
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
+-- Format code
+vim.keymap.set("n", "<leader>fm", function()
+    vim.cmd.Format()
+  end,
+  { desc = '[F]ormat', silent = true }
+)
+
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
