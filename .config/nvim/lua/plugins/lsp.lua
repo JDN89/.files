735c6bbc7f91bdadc32ca17778b72b9fd165bb79
JDN89 <jan.de.niels@gmail.com>
2025-05-24 20:51:13 +0200

    fix rust_analyzer

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,6 +1,6 @@
 local M = {}
 -- vim.lsp.enable({ "clangd" })
-vim.lsp.enable({ "lua_ls", "rust-analyzer" })
+vim.lsp.enable({ "lua_ls", "rust_analyzer" })
 
 -- Disable inlay hints initially (and enable if needed with my ToggleInlayHints command).
 vim.g.inlay_hints = false
