c656ac1d92344e403e8c8297df82276e93e752e2
Jan DeNiels <jan.deniels@axians.com>
2025-06-10 15:02:55 +0200

    add comment that explains installation lua-ls

--- .config/nvim/lsp/lua_ls.lua
+++ .config/nvim/lsp/lua_ls.lua
@@ -1,6 +1,12 @@
--- Install with
--- mac: brew install lua-language-server
--- Arch: pacman -S lua-language-server
+-- Download the .gz file that meets your architecture, don't build from source
+-- Add to ~/.local/share/lua-lang-server. 
+-- mkdir -p ~/.local/share/lua-lang-server && \
+-- tar --extract --file <YOUR .gz FILE> -av -C ~/.local/share/lua-lang-server
+-- Create a symbolic link in ~/.local/bin to the executable.
+-- ln -s ~/.local/share/lua-lang-server/bin/lua-language-server ~/.local/bin/lua-language-server
+-- Test if it works by executing lua-language-server.
+-- (Optional) If step 4 fails since bash cannot find the executable lua-language-server, then you have to append ~/.local/bin to the environment variable PATH.
+
 ---@type vim.lsp.Config
 return {
   cmd = { "lua-language-server" },
