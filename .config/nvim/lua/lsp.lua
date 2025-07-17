b72237f9fc4ec019f60fadc31332e073371f193f
Jan DeNiels <jan.deniels@axians.com>
2025-07-17 21:15:56 +0200

    remove mason and lsp_config

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -1,6 +1,5 @@
---
 -- vim.lsp.enable({ "clangd" })
-vim.lsp.enable({ "lua_ls", "rust_analyzer", "gopls", "clangd", "prettier" })
+vim.lsp.enable({ "lua_ls", "rust_analyzer" })
 
 -- Disable inlay hints initially (and enable if needed with my ToggleInlayHints command).
 vim.g.inlay_hints = false
--- .config/nvim/lua/plugins/lspconfig.lua
+++
@@ -1,5 +0,0 @@
-return {
-	"neovim/nvim-lspconfig",
-	event = { "BufReadPre", "BufNewFile" },
-	config = function() end,
-}
