5968ebcac30d1d33866eb2c66a4c939ed615c3ff
Jan DeNiels <jan.deniels@axians.com>
2025-07-17 21:37:41 +0200

    remap mini surround

--- .config/nvim/lua/plugins/mini-surround.lua
+++ .config/nvim/lua/plugins/mini-surround.lua
@@ -1,19 +1,21 @@
 -- Surround selections, add quotes, etc.
 return {
-  {
-    'kylechui/nvim-surround',
-    event = 'VeryLazy',
-    opts = {
-      keymaps = {
-        insert = false,
-        insert_line = false,
-        visual_line = false,
-        normal = 'yz',
-        normal_cur = 'yzz',
-        normal_line = 'yZ',
-        normal_cur_line = 'yZZ',
-        visual = 'Z',
-      },
-    },
-  },
+  "kylechui/nvim-surround",
+  version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+  event = "VeryLazy",
+  config = function()
+    require("nvim-surround").setup({
+      -- Configuration here, or leave empty to use defaults
+    })
+  end
 }
+
+--     Old text                    Command         New text
+-- --------------------------------------------------------------------------------
+--     surr*ound_words             ysiw)           (surround_words)
+--     *make strings               ys$"            "make strings"
+--     [delete ar*ound me!]        ds]             delete around me!
+--     remove <b>HTML t*ags</b>    dst             remove HTML tags
+--     'change quot*es'            cs'"            "change quotes"
+--     <b>or tag* types</b>        csth1<CR>       <h1>or tag types</h1>
+--     delete(functi*on calls)     dsf             function calls
