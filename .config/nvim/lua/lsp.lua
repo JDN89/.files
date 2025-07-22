c4dd9cd63e4694c9a8f03e2fa48ddf9929798bae
Jan DeNiels <jan.deniels@axians.com>
2025-07-22 14:28:00 +0200

    fix errorn source: Error encountered while sourcing file '/home/jan-deniels/.cargo/env.fish'

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -1,5 +1,5 @@
 -- vim.lsp.enable({ "clangd" })
-vim.lsp.enable({ "lua_ls", "rust_analyzer" })
+vim.lsp.enable({ "lua_ls", "rust_analyzer", "gopls" })
 
 -- Disable inlay hints initially (and enable if needed with my ToggleInlayHints command).
 vim.g.inlay_hints = false
